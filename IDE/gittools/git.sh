88e42ffc3ae56485c3122b6f5af2934762e8deda
lovelacelee <lovelacelee@gmail.com>
2016-07-29 18:45:11 +0800

    git tools

---
+++ IDE/gittools/git.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+
+
+GITROOT=/opt/nfshost/serverdisk
+GITCMD=$1
+
+function gitstatus()
+{
+    local project=$1
+    cd $project
+    git status
+    cd ..
+}
+
+function gitpush()
+{
+    local project=$1
+    cd $project
+    echo -ne "Need push ?[Y/y]: "
+    read y
+    if [ $y == "Y" ] || [ $y == "y" ];then
+    git add *
+    git commit -a
+    git push
+    fi
+    cd ..
+}
+
+function gitwork()
+{
+    local workdir=$1;
+    cd $workdir
+    local list=`ls `
+    for ford in $list
+    do
+        if [ -d $ford ];then
+            echo "=========${workdir}/${ford}========="
+            case $GITCMD in
+            status)
+                gitstatus ${workdir}/${ford}
+                ;;
+            push)
+                gitstatus ${workdir}/${ford}
+                gitpush ${workdir}/${ford}
+                ;;
+            *)
+                gitstatus ${workdir}/${ford}
+                ;;
+            esac
+        else
+            echo "Jump file ${workdir}/${ford}"
+        fi
+    done
+}
+
+function workthrough()
+{
+    local workdir=$1;
+    cd $workdir
+    local list=`ls `
+    for ford in $list
+    do
+        if [ -d $ford ];then
+            gitwork ${workdir}/${ford}
+            cd ..
+        else
+            echo "Jump file ${workdir}/${ford}"
+        fi
+    done
+}
+
+
+
+## main
+
+# show tree
+tree -L 2 -d $GITROOT
+
+workthrough $GITROOT
+
+
